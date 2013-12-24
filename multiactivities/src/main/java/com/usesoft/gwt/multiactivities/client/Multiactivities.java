6221ae7de17dcbc156291280ef8998596edcc6fc
Ronan Quillevere <ronan.quillevere@gmail.com>
2013-12-24 19:35:30 +0100

    first commit

---
+++ multiactivities/src/main/java/com/usesoft/gwt/multiactivities/client/Multiactivities.java
@@ -0,0 +1,12 @@
+package com.usesoft.gwt.multiactivities.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+
+public class Multiactivities implements EntryPoint {
+
+    public void onModuleLoad() {
+        RootLayoutPanel.get().add(new Label("Hello World"));
+    }
+}
